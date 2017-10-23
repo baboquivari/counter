12bb6df38b5aad3105df1d4cd4412a31040a5992
unknown <to.sian.san@gmail.com>
2017-10-23 12:02:24 +0100

    set up counter component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Counter extends React.Component {
+    render () {
+      return (
+        <div className="counter">
+          {this.props.text}
+          {": "}
+          <span className="counter-value">0</span>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      );
+    }
+  }
+
+ReactDOM.render(
+  <Counter text="COUNTER"/>,
+  document.getElementById('root')
+);
